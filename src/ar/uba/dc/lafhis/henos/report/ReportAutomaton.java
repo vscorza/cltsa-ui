c0e281ddcb5770e92cd1bc16498a858861c5b31d
Mariano Cerrutti <vscorza@gmail.com>
2018-10-22 16:46:59 -0300

    implementing context report

--- src/ar/uba/dc/lafhis/henos/report/ReportAutomaton.java
+++ src/ar/uba/dc/lafhis/henos/report/ReportAutomaton.java
@@ -1,5 +1,83 @@
 package ar.uba.dc.lafhis.henos.report;
 
-public class ReportAutomaton {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ReportAutomaton extends ReportObject{
+	protected String name;
+	protected List<Integer> localAlphabet;
+	protected List<ReportTransition> transitions;
+	protected List<Integer> initialStates;
+	protected List<List<Integer>> fluentValuations;
+	protected List<List<Integer>> livenessValuations;
+	
+	public String getName() {return name;}
+	public List<Integer> getLocalAlphabet(){return localAlphabet;}
+	public List<ReportTransition> getTransitions(){return transitions;}
+	public List<Integer> getInitialStates(){return initialStates;}
+	public List<List<Integer>> getFluentValuations(){return fluentValuations;}
+	public List<List<Integer>> getLivenessValuations(){return livenessValuations;}
+	/*
+	 * <name,ctx,local_alphabet_count,[sig_1.idx,..,sig_N.idx],trans_count,[trans_1,..,trans_M],init_count,[init_i,..,init_K],[[val_s_0_f_0,..,val_s_0_f_L],..,[val_s_T_f_0,..,val_s_T_f_L]]
+	,[[val_s_0_l_0,..,val_s_0_l_R],..,[val_s_T_l_0,..,val_s_T_l_R]]>
+	 */
+	public ReportAutomaton(FileInputStream fis) {
+		localAlphabet	= new ArrayList<Integer>();
+		transitions		= new ArrayList<ReportTransition>();
+		initialStates	= new ArrayList<Integer>();
+		fluentValuations	= new ArrayList<List<Integer>>();
+		livenessValuations	= new ArrayList<List<Integer>>();
+	    try {
+			char current;
+			current = (char) fis.read();
+			if(current != ReportConstants.AUT_SER_OBJ_START) {
+				isOK = false;
+				return;
+			}
+			name			= readString(fis, ReportConstants.AUT_SER_SEP);
+			/*
+			alphabet		= new ReportAlphabet(fis);
+			current = (char) fis.read();
+			if(current != ReportConstants.AUT_SER_SEP) {
+				isOK = false;
+				return;
+			}
+			int count	= readInt(fis, ReportConstants.AUT_SER_SEP);
+			current 	= (char) fis.read();
+			if(current != ReportConstants.AUT_SER_ARRAY_START) {
+				isOK	= false;
+				return;
+			}			
+			if(count == 0) {
+				current = (char) fis.read();
+				if(current != ReportConstants.AUT_SER_ARRAY_END) {
+					isOK	= false;
+					return;
+				}		
+			}else {
+				fluents	= readStringArray(fis, ReportConstants.AUT_SER_OBJ_END);
+			}
+			count		= readInt(fis, ReportConstants.AUT_SER_SEP);
+			if(count == 0) {
+				current 	= (char) fis.read();
+				if(current != ReportConstants.AUT_SER_ARRAY_START) {
+					isOK	= false;
+					return;
+				}			
+				current = (char) fis.read();
+				if(current != ReportConstants.AUT_SER_ARRAY_END) {
+					isOK	= false;
+					return;
+				}
+			}else {
+				livenessNames	= readStringArray(fis, ReportConstants.AUT_SER_OBJ_END);
+			}		
+			*/	
+	      } catch (IOException e) {
+	    	  isOK = false;
+	    	  e.printStackTrace();
+	      }		
+	}
 }
--- src/ar/uba/dc/lafhis/henos/report/ReportContext.java
+++ src/ar/uba/dc/lafhis/henos/report/ReportContext.java
@@ -8,17 +8,17 @@ import java.util.List;
 public class ReportContext extends ReportObject {
 	protected String name;
 	protected ReportAlphabet alphabet;
-	protected List<ReportFluent> fluents;
+	protected List<String> fluents;
 	protected List<String> livenessNames;
 	
 	
 	public String getName() {return name;}
 	public ReportAlphabet getAlphabet() { return alphabet;}
-	public List<ReportFluent> getfluents(){return fluents;}
+	public List<String> getfluents(){return fluents;}
 	public List<String> getLivenessNames(){return livenessNames;}
 	//<name,alphabet,fluents_count,[f_1.name,..,f_n.name],liveness_count,[l_1.name,..,l_m.name]>
 	public ReportContext(FileInputStream fis) {
-		fluents			= new ArrayList<ReportFluent>();
+		fluents			= new ArrayList<String>();
 		livenessNames	= new ArrayList<String>();
 	    try {
 			char current;
@@ -47,16 +47,7 @@ public class ReportContext extends ReportObject {
 					return;
 				}		
 			}else {
-				do {
-					fluents.add(new ReportFluent(fis));
-					current = (char) fis.read();
-					if(current != ReportConstants.AUT_SER_ARRAY_END && current
-							!= ReportConstants.AUT_SER_SEP) {
-						isOK	= false;
-						return;
-					}
-						
-				}while(current != ReportConstants.AUT_SER_ARRAY_END);
+				fluents	= readStringArray(fis, ReportConstants.AUT_SER_OBJ_END);
 			}
 			count		= readInt(fis, ReportConstants.AUT_SER_SEP);
 			if(count == 0) {
